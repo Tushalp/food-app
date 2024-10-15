79b92acd59b80677ebc308b9489acaac668d3f8e
Tushal Patel <ptushal53@gmail.com>
2024-10-15 10:13:42 +0530

    check and correct some error

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-// const colors = require("colors");
 
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Connected To Database ${mongoose.connection.host} `);
+    console.log(`Connected To Database  `);
+    // console.log(`Connected To Database ${mongoose.connection.host} `);
   } catch (error) {
     console.log("DB Error", error);
   }
